6a8a0b027e872c5aeaf4c57ace0567ff52e0ce60
Jack Huey <kitskub@yahoo.com>
2012-04-01 20:47:24 -0500

    More stuff

--- src/main/java/com/thedevteam/thecore/THECore.java
+++ src/main/java/com/thedevteam/thecore/THECore.java
@@ -1,8 +1,18 @@
 package com.thedevteam.thecore;
 
+import org.spout.api.plugin.CommonPlugin;
+
 /**
  * Core class
  *
  */
-public class THECore {
+public class THECore extends CommonPlugin{
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
 }
