1210fdb34b1b27de1e28b5de5df18c739cfed89e
Jack Huey <kitskub@yahoo.com>
2012-04-02 21:06:09 -0500

    Material stuff.

---
+++ src/main/java/com/thedevteam/thecore/materials/Materials.java
@@ -0,0 +1,26 @@
+package com.thedevteam.thecore.materials;
+
+import org.spout.api.material.Material;
+import org.spout.api.material.MaterialRegistry;
+
+/**
+ *
+ *
+ */
+public abstract class Materials {
+    
+    @SuppressWarnings("unchecked")
+    protected static <T extends Material> T register(T material) {
+        String name = material.getName();
+        Material registered = MaterialRegistry.get(name);
+        if(registered != null){
+            try{
+               T t = (T) material.getClass().cast(registered);
+               return t;
+            }
+            catch(ClassCastException e){
+            }
+        }
+        return MaterialRegistry.register(material);
+    }
+}
